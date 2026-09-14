5d57f184d39901f1bca8cec59d5e193e8678acbc
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-14 16:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -54,3 +54,46 @@ function renderQuestions() {
   }
 }
 renderQuestions();
+const questionsElement = document.getElementById("questions");
+const submitButton = document.getElementById("submit");
+const scoreElement = document.getElementById("score");
+
+// Get saved answers from sessionStorage
+let userAnswers = JSON.parse(sessionStorage.getItem("progress")) || {};
+
+// Save answer whenever a radio button is selected
+document.addEventListener("change", function (event) {
+  if (event.target.type === "radio") {
+    const questionIndex = event.target.name.split("-")[1];
+
+    userAnswers[questionIndex] = event.target.value;
+
+    sessionStorage.setItem(
+      "progress",
+      JSON.stringify(userAnswers)
+    );
+  }
+});
+
+// Submit quiz
+submitButton.addEventListener("click", function () {
+  let score = 0;
+
+  for (let i = 0; i < questions.length; i++) {
+    if (userAnswers[i] === questions[i].answer) {
+      score++;
+    }
+  }
+
+  scoreElement.innerText = `Your score is ${score} out of 5.`;
+
+  // Save score in localStorage
+  localStorage.setItem("score", score);
+});
+
+// Restore previous score
+const savedScore = localStorage.getItem("score");
+
+if (savedScore !== null) {
+  scoreElement.innerText = `Your score is ${savedScore} out of 5.`;
+}
